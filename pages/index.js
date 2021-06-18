aaad93c6a28ba73ded6fbffa0e34d49cc37bd279
James Wachmann <james.wachmann@gmail.com>
2021-06-18 16:09:40 -0400

    WIP

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -10,12 +11,24 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Read{' '}
+          <Link href="/posts/first-post">
+            <a>this page!</a>
+          </Link>
         </h1>
 
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+
+        <div className="navigation">
+          <h2>Navigation</h2>
+          <ul>
+            <li>
+              <a href="posts/first-post">First Post</a>
+            </li>
+          </ul>
+        </div>
 
         <div className="grid">
           <a href="https://nextjs.org/docs" className="card">
@@ -67,6 +80,11 @@ export default function Home() {
           flex-direction: column;
           justify-content: center;
           align-items: center;
+        }
+
+        .navigation {
+          align-self: flex-start;
+          margin: 0 16px;
         }
 
         main {
